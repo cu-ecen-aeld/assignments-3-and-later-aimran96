75a33a784c540741ca2953ded78aecab5c7ebab1
aimran96 <ad_min96@outlook.com>
2023-10-24 14:22:03 -0400

    add script demo to trigger automated tests

--- filepath_and_filename.sh
+++ filepath_and_filename.sh
@@ -1,3 +1,4 @@
+#https://stackoverflow.com/questions/6121091/how-to-extract-directory-path-from-file-path
 VAR='/home/pax/file.c'
 DIR="$(dirname "${VAR}")" ; FILE="$(basename "${VAR}")"
 echo "[${DIR}] [${FILE}]"
