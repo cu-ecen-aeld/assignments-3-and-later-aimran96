1042a66035a0b35e5932c158d0f212adf20b5176
aimran96 <ad_min96@outlook.com>
2023-11-06 17:58:54 -0500

    add C logic for open() and write(); TODO: error checking and logging

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-int main(){
-	printf("Hello world!\n");
-	return 0;
+int main( int argc, char *argv[] )  {
+
+   if( argc == 3 ) {
+   	printf("The argument supplied is 1) %s 2) %s\n", argv[1], argv[2]);
+   	int outputFd;
+	outputFd = open(argv[1], O_WRONLY | O_CREAT , 0666);
+	if(outputFd == -1) {
+	    /* Error handling */
+	    return -1;
+	}
+	ssize_t nr;
+	/* write the string in 'buf' to 'fd' */
+	nr = write (outputFd, argv[2], strlen (argv[2]));
+	
+   }
+   else if( argc > 3 ) {
+      printf("Too many arguments supplied.\n");
+      printf("Two arguments expected.\n");
+   }
+   else {
+      printf("Two arguments expected.\n");
+   }
 }
