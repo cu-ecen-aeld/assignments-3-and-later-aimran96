59efa671f0a76dd4541d76dcbdb96c0f325ac800
aimran96 <ad_min96@outlook.com>
2023-11-12 17:56:09 -0500

    Add final makefile and related files

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -7,7 +7,7 @@ set -u
 
 OUTDIR=/tmp/aeld
 KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
-KERNEL_VERSION=v5.1.10
+KERNEL_VERSION=v6.1.14
 BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
@@ -33,11 +33,17 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
-
-    # TODO: Add your kernel build steps here
+    # https://unix.stackexchange.com/questions/657882/how-do-i-cross-compile-the-linux-kernel
+    #make ARCH="$ARCH" CROSS_COMPILE="$CROSS_COMPILE" menuconfig
+    #make ARCH="$ARCH" CROSS_COMPILE="$CROSS_COMPILE" config
+    # use default config
+    make ARCH=arm64 CROSS_COMPILE="$CROSS_COMPILE" defconfig
+    make ARCH="$ARCH" CROSS_COMPILE="$CROSS_COMPILE"
 fi
 
 echo "Adding the Image in outdir"
+
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -48,6 +54,25 @@ then
 fi
 
 # TODO: Create necessary base directories
+mkdir rootfs/
+mkdir rootfs/bin
+mkdir rootfs/dev
+mkdir rootfs/etc
+mkdir rootfs/home
+mkdir rootfs/lib
+mkdir rootfs/lib/modules
+mkdir rootfs/lib64
+mkdir rootfs/lib64/modules
+mkdir rootfs/proc
+mkdir rootfs/sys
+mkdir rootfs/sbin
+mkdir rootfs/tmp
+mkdir rootfs/usr
+mkdir rootfs/usr/lib
+mkdir rootfs/usr/bin 
+mkdir rootfs/usr/sbin
+mkdir rootfs/var
+mkdir rootfs/var/log 
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -56,25 +81,53 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    # https://busybox.net/FAQ.html#configure
+    make ARCH="$ARCH" CROSS_COMPILE="$CROSS_COMPILE" defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+make ARCH="$ARCH" CROSS_COMPILE="$CROSS_COMPILE"
+make ARCH="$ARCH" CROSS_COMPILE="$CROSS_COMPILE" CONFIG_PREFIX="${OUTDIR}/rootfs" install
+cd ..
+pwd
 
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "program interpreter"
+${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+cp ~/arm-cross-compiler/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
+cp ~/arm-cross-compiler/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64
+cp ~/arm-cross-compiler/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64
+cp ~/arm-cross-compiler/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64
 
 # TODO: Make device nodes
+cd "$OUTDIR/rootfs"
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 600 dev/console c 5 1
 
 # TODO: Clean and build the writer utility
+cd "$FINDER_APP_DIR"
+make clean
+make ARCH="$ARCH" CROSS_COMPILE="$CROSS_COMPILE"
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp writer ${OUTDIR}/rootfs/home
+cp finder.sh ${OUTDIR}/rootfs/home
+cp finder-test.sh ${OUTDIR}/rootfs/home
+cp writer.sh ${OUTDIR}/rootfs/home
+mkdir ${OUTDIR}/rootfs/home/conf
+cp -r conf/. ${OUTDIR}/rootfs/home/conf
+cp autorun-qemu.sh ${OUTDIR}/rootfs/home
 
 # TODO: Chown the root directory
+cd "$OUTDIR/rootfs"
+sudo chown -R root:root *
 
 # TODO: Create initramfs.cpio.gz
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+cd "$OUTDIR"
+gzip -f initramfs.cpio
--- setup.sh
+++ setup.sh
@@ -6,3 +6,6 @@ sudo apt install -y make
 sudo apt-get install build-essential
 sudo apt-get install ruby-full
 
+sudo apt-get install git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison
+sudo apt install qemu-system-arm qemu qemu-user
+
